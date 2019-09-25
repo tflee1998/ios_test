11e7ba1aae19923b706b1f8c3aad90eb48e378dd
cherry880807 <cherry880807@yahoo.com.tw>
2019-09-25 10:41:12 +0800

    Initial Commit

---
+++ ios_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_test
+//
+//  Created by User02 on 2019/9/25.
+//  Copyright © 2019 YuYu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
